a2bbb0b57f6282d140c96dc5f64a8969e8281ad7
shitanshu-277 <shitanshugoyal2002@gmail.com>
2023-12-16 02:32:28 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+
+app.get('/home',(req,res)=>{
+    res.json({message:"ok"});
+})
+app.listen(3000,()=>{
+    console.log("server is listening on port 3000");
+})
